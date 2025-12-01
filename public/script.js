53d6be5f4a9b3b1c39487a72dbd88bf1037284a9
elga21 <fernando.bevans@gmail.com>
2025-12-01 18:07:35 -0500

    Fix: Update API URL for production

--- public/script.js
+++ public/script.js
@@ -1,4 +1,7 @@
-const API_URL = 'http://localhost:3000/api';
+// Detect environment and use correct API URL
+const API_URL = window.location.hostname === 'localhost'
+    ? 'http://localhost:3000/api'
+    : `${window.location.origin}/api`;
 
 let isLogin = true;
 
